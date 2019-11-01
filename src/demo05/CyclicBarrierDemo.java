82a71c97f8ded89995f645dc854f2b851009338c
lidandan <13803345745@163.com>
2019-11-01 17:29:54 +0800

    fix CyclicBarrier

---
+++ src/demo05/CyclicBarrierDemo.java
@@ -0,0 +1,24 @@
+package demo05;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println(Thread.currentThread().getName() + "\t召唤神龙");
+        });
+        for (int i = 0; i < 7; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t集齐\t" + tempInt);
+                try {
+                    cyclicBarrier.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                }
+            }, String.valueOf(i)).start();
+        }
+
+    }
+}
